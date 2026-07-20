c3df2df3d4b09eb27d1e94dc6308eced547c190c
Flemming Eriksson <fleeri91@gmail.com>
2026-07-20 13:42:45 +0200

    tabwidth fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import queryClient from './lib/query-client.ts'
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
-    </StrictMode>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
 )
