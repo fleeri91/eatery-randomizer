a5821744f6d006d6e893aeb5951592b387d3f0fd
Flemming Eriksson <fleeri91@gmail.com>
2026-07-20 17:11:27 +0200

    start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,41 @@
+import { useState } from 'react'
+import { StartScreen } from './views/start-screen'
+import { useCityLocation, useNearbyPlaces } from './lib/queries'
+import { type PlaceFilterValues } from './types/google-places'
+
 export default function App() {
-  return <></>
+  const [submitted, setSubmitted] = useState<{
+    city: string
+    filters: PlaceFilterValues
+  } | null>(null)
+
+  const {
+    data: location,
+    isLoading: geocoding,
+    error: geoError,
+  } = useCityLocation(submitted?.city ?? '')
+  const {
+    data: places,
+    isLoading: searching,
+    error: placesError,
+  } = useNearbyPlaces(
+    location,
+    submitted?.filters.category ?? 'cafe',
+    submitted?.filters.radiusMeters
+  )
+
+  function handleSubmit(city: string, filters: PlaceFilterValues) {
+    setSubmitted({ city, filters })
+  }
+
+  return (
+    <>
+      {!submitted && <StartScreen onSubmit={handleSubmit} />}
+      {submitted && (geocoding || searching) && <p>Loading...</p>}
+      {submitted && (geoError || placesError) && (
+        <p style={{ color: 'red' }}>{(geoError ?? placesError)?.message}</p>
+      )}
+      {submitted && places && <pre>{JSON.stringify(places, null, 2)}</pre>}
+    </>
+  )
 }
